d04d3e23f3ed0cd897de1f4c69d4561bb56616d2
Mark de Bruijn <mrdebruijn@gmail.com>
2020-05-13 20:18:15 +0200

    Make Autopep8 Happy, fix default output directory when none is given, make unittest run with hyperbased.mod

--- lib/modtag/modtag.py
+++ lib/modtag/modtag.py
@@ -1,40 +1,40 @@
-﻿import struct
-from struct import unpack
-from lib.modtag.format import ModuleFormat, ProtrackerFormat
+﻿from lib.modtag.format import ProtrackerFormat
 
 formats = []
 formats.append(ProtrackerFormat())
 
+
 class InvalidModuleException(Exception):
-	pass
+    pass
+
 
 def detect_module_format(modulebytes):
-	for f in formats:
-		if f.identify(modulebytes):
-			return f
+    for f in formats:
+        if f.identify(modulebytes):
+            return f
 
-	return None
+    return None
+
 
 def load_module(modulebytes, options=None):
-	f = detect_module_format(modulebytes)
+    f = detect_module_format(modulebytes)
 
-	if f == None:
-		raise InvalidModuleException()
+    if f is None:
+        raise InvalidModuleException()
 
-	return f.load_module(modulebytes, options)
+    return f.load_module(modulebytes, options)
+
 
 def get_pattern_string(song, pnum):
-	pattern = song.patterns[pnum]
-	print("Pattern #" + str(pnum))
-	print("len: " + str(pattern.length))
-	print("channels: " + str(song.num_channels))
+    pattern = song.patterns[pnum]
+    print("Pattern #" + str(pnum))
+    print("len: " + str(pattern.length))
+    print("channels: " + str(song.num_channels))
 
-	for r in range(pattern.length-1):
-		for c in range(song.num_channels-1):
-			#print("pos {0} {1}", c, r)
-			note = pattern.rows[c][r]
-			print("\t|", note.pitch, hex(note.effect), hex(note.parameters), end="")
-		print()
-
-
-
+    for r in range(pattern.length - 1):
+        for c in range(song.num_channels - 1):
+            # print("pos {0} {1}", c, r)
+            note = pattern.rows[c][r]
+            print("\t|", note.pitch, hex(note.effect), hex(note.parameters),
+                  end="")
+        print()
--- ripper.py
+++ ripper.py
@@ -5,12 +5,13 @@ import argparse
 import hashlib
 import string
 import wave
-from lib.modtag.modtag import load_module
 
+from lib.modtag.modtag import load_module
 
 parser = argparse.ArgumentParser(description='Rip samples from Amiga modules')
 parser.add_argument('module', help='The module where to rip samples from')
-parser.add_argument('--output', help='Where to save the resulting samples', default='.')
+parser.add_argument('--output', help='Where to save the resulting samples',
+                    default='')
 parser.add_argument('--verbose', action='store_true')
 parser.add_argument('--rate', default=8000)
 
@@ -23,23 +24,22 @@ songbytes = f.read()
 f.close()
 
 mod = load_module(songbytes, {'verbose': args.verbose})
-rate = args.rate 
+rate = args.rate
 
 for ins in mod.instruments:
-	s = ins.sample
-	if s.length == 0:
-		continue
+    s = ins.sample
+    if s.length == 0:
+        continue
 
-	name = ''.join(c for c in ins.sample.name if c in valid_chars)
-	
-	if len(name) == 0:
-		# use md5 hash of the sample data if no name given
-		# not tested
-		m = hashlib.md5(s.data)
-		name = m.hexdigest()
+    name = ''.join(c for c in ins.sample.name if c in valid_chars)
 
-	w = wave.open(args.output + name + ".wav", 'w')
-	w.setparams((1, 2, int(rate), 0, 'NONE', 'not compressed'))
-	w.writeframes(s.data)
-	w.close()
+    if len(name) == 0:
+        # use md5 hash of the sample data if no name given
+        # not tested
+        m = hashlib.md5(s.data)
+        name = m.hexdigest()
 
+    w = wave.open(args.output + name + ".wav", 'w')
+    w.setparams((1, 2, int(rate), 0, 'NONE', 'not compressed'))
+    w.writeframes(s.data)
+    w.close()
